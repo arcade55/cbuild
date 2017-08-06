c0f6aef7d539f646aba579f8d97406b44822d177
Richard Knowler <richardknowler@Richards-iMac.local>
2017-08-06 15:21:25 +1200

    updated text

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello secure World!")
+	io.WriteString(w, "Hello secure World on this day 6th August 2017!")
 }
 
 func main() {
